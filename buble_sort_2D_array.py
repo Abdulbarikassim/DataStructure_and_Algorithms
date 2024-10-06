9ea641142264b05d2df953c4882f289b4a4ead6e
= <=>
2024-10-06 13:56:24 +0400

    updated the 2D bubble sort function

--- buble_sort_2D_array.py
+++ buble_sort_2D_array.py
@@ -2,9 +2,9 @@
 
 def sort(nums):
     for row in nums:
-        for i in range(len(nums) - 1):
+        for i in range(len(row) - 1):
             swapped = False
-            for j in range(len(nums) - i - 1):
+            for j in range(len(row) - i - 1):
                 if row[j] > row[j + 1]:
                     temp = row[j]
                     row[j] = row[j + 1]
@@ -15,11 +15,10 @@ def sort(nums):
 
 
 arr = [
-    [9, 3, 1,11,14,78,8,12,5],
-    [4, 7, 2,9,8,4,5,3,13,2],
-    [8, 5, 6,14,23,43,54,21,2]
+    [9, 3, 1, 11, 14, 78, 8, 12, 5],
+    [4, 7, 2, 9, 8, 4, 5, 3, 13, 2],
+    [8, 5, 6, 14, 23, 43, 54, 21, 2]
 ]
 
 sort(arr)
 print(arr)
-
