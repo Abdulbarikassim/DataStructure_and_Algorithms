2c09f16b1921932f067cfa8df583918d933dd673
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-10-08 14:32:16 +0400

    Func: Added select_sort algorithm.
    Added the select sort algorithm which goes throughs all the values in
    the array and the select the smallest value and brings it the front.

--- selection_sort.py
+++ selection_sort.py
@@ -1,17 +1,38 @@
 # selections sort is an algorithm that selects the values value in the array and moves in the front.
 from typing import List
 
+
 # First you go through the array and the first the lowest value.
 # and then move the to front.
-# and then iterate starting from the second index and then check then next small value send it to the front.
+# and then iterate starting from the second index and then check next small value send it to the front.
 # continue until all the array is sorted.
 
-# Implementing the selection sort:
-# outer loop checks how many times the inner loop runs.
-# inner loop goes through everything and then  moves the smallest value to the front.
 
-nums = [3, 7, 9, 11, 12]
+# what are the steps that are needed to implement this in a code:
+# You get an unsorted array.
+# An inner loop that goes through the array, finds the lowest value, brings to the front.
+# This loop must loop through one less value each time runs.
+# An outer loop that controls how many times the inner loop must run. For an array with n
+# value, this outer loop must run `n-1` times.
 
-n = len(nums)
+
+# let n = 5
+# we will loop 4 times which mean from 0-4:
+# and perform what functions:
+# get the let just say the state for the min_index =
+
+def selection_sort(nums):
+    n = len(nums)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        #     swap
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
 
 
+numbers = [7, 2, 8, 3, 9, 4, 10, 5]
+selection_sort(numbers)
+print(numbers)
