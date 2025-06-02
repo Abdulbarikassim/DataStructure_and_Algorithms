aa48f4d705d3ab025f445b5038a7562fa23c860e
Abdulbarikassim <abdicsxhaka10@gmail.com>
2025-06-02 18:06:13 +0400

    Added method that returns the longest string.

---
+++ longest_word.py
@@ -0,0 +1,18 @@
+"""
+Find the longest word in the sentence and if there is a tier return the later word. 
+"""
+
+def longest_word(sentence):
+    longest = ""
+    split_sent  = sentence.split()
+    print(split_sent)
+    for word in split_sent: 
+        if  len(word) >= len(longest): 
+            longest = word 
+    return longest 
+
+
+sentence = "Find the longest word in the sentence and if there is a tier return the later word" 
+print("Longest word: ", longest_word(sentence))
+
+
