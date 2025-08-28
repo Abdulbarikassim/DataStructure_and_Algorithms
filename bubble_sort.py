6b025d32326c959f406782295f8fcb0614ee9d0e
Abdulbarikassim <abdicsxhaka10@gmail.com>
2025-08-28 09:33:14 +0400

    added different way to sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,27 +7,41 @@
 # and if everything in array is the same.
 # This code now solve both positive and negative integers in a list.
 
-def sort(nums):
-    try:
-        if len(nums) <= 0 or len(nums) < 1:
-            raise ValueError("Number should be more than two for comparison")
-        elif all(x == nums[0] for x in nums):
-            raise ValueError(f"Cannot sort if all the values of the array are equal")
-        # outer loop.
-        for i in range(len(nums) - 1):
-            swapped = False
-            for j in range(len(nums) - i - 1):
-                if nums[j] > nums[j + 1]:
-                    temp = nums[j]
-                    nums[j] = nums[j + 1]
-                    nums[j + 1] = temp
-                    swapped = True
+# def sort(nums):
+#     try:
+#         if len(nums) <= 0 or len(nums) < 1:
+#             raise ValueError("Number should be more than two for comparison")
+#         elif all(x == nums[0] for x in nums):
+#             raise ValueError(f"Cannot sort if all the values of the array are equal")
+#         # outer loop.
+#         for i in range(len(nums) - 1):
+#             swapped = False
+#             for j in range(len(nums) - i - 1):
+#                 if nums[j] > nums[j + 1]:
+#                     temp = nums[j]
+#                     nums[j] = nums[j + 1]
+#                     nums[j + 1] = temp
+#                     swapped = True
+#
+#             if not swapped:
+#                 break
+#     except Exception as e:
+#         print(e)
+#
 
-            if not swapped:
-                break
-    except Exception as e:
-        print(e)
+def sort(arr):
+    swapped  = True
+    end = len(arr)
 
+    while swapped:
+        swapped =  False
+        for i in range(1, end):
+            if arr[i-1] > arr[i]:
+                arr[i-1], arr[i] = arr[i] , arr[i-1]
+
+                swapped = True
+        end -= 1
+    return arr
 
 numbers = [4, 2, 7, 5, 12, 10, 8, 6]
 
